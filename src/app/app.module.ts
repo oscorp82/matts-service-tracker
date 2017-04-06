de3c1075c7a814f3e508dba57d126d93bfc21414
Matthew <matthew.n.osborne@gmail.com>
2017-04-06 12:45:37 -0400

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+var firebaseDatabaseConfig = {
+  apiKey: "AIzaSyBgVypNqnFjFt5u__PwLAOwhibOIwWss1o",
+  authDomain: "matt-service-tracker.firebaseapp.com",
+  databaseURL: "https://matt-service-tracker.firebaseio.com",
+  projectId: "matt-service-tracker",
+  storageBucket: "matt-service-tracker.appspot.com",
+  messagingSenderId: "553272991541"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
@@ -15,6 +30,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseDatabaseConfig, firebaseAuthConfig),
     MaterialModule,
   ],
   providers: [],
